5e985675fa51875eae637e50b3170103352909ca
Linh Le <dev.linhlv@gmail.com>
2016-11-16 17:26:07 +0700

    Boot

--- core/cp/public/core/cp/app/bootstrap.js
+++ core/cp/public/core/cp/app/bootstrap.js
@@ -2,7 +2,7 @@
 define([], function(){ 
     $.get('/cp/spa_metadata.json', function(metadata){
         if(metadata && metadata.modules){
-            var pathModules = [], moduleRoot = 'modules/', refModules = [];
+            var pathModules = [], moduleRoot = 'modules/', refModules = [], metaModules = [];
             window.spa = window.spa || {};
             window.spa.modules = {};
 
@@ -16,9 +16,12 @@ define([], function(){
                 var _mp = moduleRoot + m.name;
                 refModules.push(m.name);
                 pathModules.push(_mp + '/loader');
+                metaModules.push(_mp + '/module.json');
             });            
 
-            require(pathModules, function(){                   
+            require(pathModules, function(){     
+                //$.when()
+
                 angular.module(window.spa.name, ['shell']);
                 angular.element(function() {
                     angular.bootstrap(document, [window.spa.name]);

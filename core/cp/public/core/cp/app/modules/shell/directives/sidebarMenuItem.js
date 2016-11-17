fe06c5e47c4b03fc966f4abd9843696580c4d455
Linh Le <dev.linhlv@gmail.com>
2016-11-17 17:24:42 +0700

    icon is working

--- core/cp/public/core/cp/app/modules/shell/directives/sidebarMenuItem.js
+++ core/cp/public/core/cp/app/modules/shell/directives/sidebarMenuItem.js
@@ -10,21 +10,20 @@ define(function () {
                 item: "="
             },           
             link: function(scope, element, attr) {
-                if(angular.isArray(scope.item.childitems)){
-                    element.html('');
+                element.html('');
+                if(angular.isArray(scope.item.childitems)){                   
                     element.attr('class', 'sub-menu')           
-                    element.append('<a toggle-submenu>{{item.text}}</a>');
+                    element.append('<a toggle-submenu><i class="zmdi zmdi-widgets"></i> {{item.text}}</a>');
                     element.append('<ul><sidebar-menu-item ng-repeat="childitem in item.childitems" item="childitem"></sidebar-menu-item></ul>');                    
                 }else{
-                    element.html('');
-                    element.append('<a>{{item.text}}</a>');
+                    element.append('<a><i class="zmdi zmdi-widgets"></i> {{item.text}}</a>');
                 }
 
                 $compile(element.contents())(scope);
 
                 console.log("treeview item directive loaded");
             },
-            template: '<li data-ui-sref-active="active"><a>{{item.text}}</a></li>'
+            template: '<li data-ui-sref-active="active"></li>'
         };       
     }
 

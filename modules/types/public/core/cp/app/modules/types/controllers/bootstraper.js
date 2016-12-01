37970425b4bb25d39e29e83d6dba2dacc0b78aab
Linh Le <linh.le@smoovapp.com>
2016-12-01 18:01:27 +0700

    Edit tab navigation

--- modules/types/public/core/cp/app/modules/types/controllers/bootstraper.js
+++ modules/types/public/core/cp/app/modules/types/controllers/bootstraper.js
@@ -2,12 +2,14 @@ define(['modules/types/controllers/types-list.ctrl',
 'modules/types/controllers/types-add.1.ctrl',  
 'modules/types/controllers/types-add.2.ctrl',
 'modules/types/controllers/types-add.3.ctrl',
-'modules/types/controllers/types-add.4.ctrl'],
-    function (types_list, types_add_1, types_add_2, types_add_3, types_add_4) {
+'modules/types/controllers/types-add.4.ctrl',
+'modules/types/controllers/types-edit.ctrl'],
+    function (types_list, types_add_1, types_add_2, types_add_3, types_add_4, types_edit) {
         angular.module('types.controllers', [])
             .controller('types-list.ctrl', ['$scope', '$state', '$sessionStorage', 'types.svc', types_list])
             .controller('types-add.1.ctrl', ['$scope', '$state', '$sessionStorage', types_add_1])        
             .controller('types-add.2.ctrl', ['$scope', '$state', '$sessionStorage', types_add_2])        
             .controller('types-add.3.ctrl', ['$scope', '$state', '$sessionStorage', types_add_3])        
-            .controller('types-add.4.ctrl', ['$scope', '$state', '$sessionStorage', 'types.svc', types_add_4]);
+            .controller('types-add.4.ctrl', ['$scope', '$state', '$sessionStorage', 'types.svc', types_add_4])
+            .controller('types-edit.ctrl', ['$scope', '$state', '$sessionStorage', '$stateParams', 'types.svc', types_edit]);
 });
--- modules/types/public/core/cp/app/modules/types/controllers/types-edit.ctrl.js
+++ modules/types/public/core/cp/app/modules/types/controllers/types-edit.ctrl.js
@@ -2,15 +2,15 @@
 // monitor common task change
 /// </summary>
 define(function () {
-    function c($scope, $state, $sessionStorage, typesSvc){
+    function c($scope, $state, $sessionStorage, $stateParams, typesSvc){
         var vm = this;
-
-        vm.edit = function(item){$state.go('types.edit.general', {id: item._id});}
+        vm.includes = function(s){ return $state.includes(s); };
+        vm.tabSelect = function(s){ $state.go('types.edit.' + s, $stateParams.id); };
         
         typesSvc.getAll().then(function(res){
             if(res && res.items){
                 vm.items = res.items;                    
-                console.log('type list', vm.items);
+                console.log('type edit', vm.items);
             }            
         });
     }

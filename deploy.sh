1420479492db50cf53984352b10e5583477df614
Linh Le <dev.linhlv@gmail.com>
2016-12-07 17:42:06 +0700

    test deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+if [ $TRAVIS_BRANCH == 'at' ] ; then
+    # Initialize a new git repo in _site, and push it to our server.
+    cd _site
+    git init
+        
+    git remote add deploy "deploy@139.59.36.122:/var/www/at.labs.appiume.com"
+    git config user.name "Linh Le"
+    git config user.email "dev.linhlv@gmail.com"
+    
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy master
+else
+    echo "Not deploying, since this branch isn't master."
+fi

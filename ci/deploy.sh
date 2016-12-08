255b575893df9cdf3803317d815dd3355860bbb6
Linh Le <dev.linhlv@gmail.com>
2016-12-08 11:55:39 +0700

    Check push deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 echo "Deploying"
 set -x
-if [ $TRAVIS_BRANCH == 'at' ] ; then
-    git init
-
-    git remote add staging "ssh://deploy@139.59.36.122:/var/www/at.labs.appiume.com/site.git"
-    git config user.name "Linh Le"
-    git config user.email "dev.linhlv@gmail.com"
-    
-    git add .
-    git commit -m "Deploy"    
+if [ $TRAVIS_BRANCH == 'at' ] ; then    
+    git remote add staging "ssh://deploy@139.59.36.122:/var/www/at.labs.appiume.com/site.git" 
     git push staging master
 else
     echo "Not deploying, since this branch isn't master."

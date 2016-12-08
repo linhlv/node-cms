08379b2ea1b8771f7625b35e66f784043972de70
Linh Le <dev.linhlv@gmail.com>
2016-12-08 11:21:09 +0700

    update travis

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,7 +6,7 @@ if [ $TRAVIS_BRANCH == 'at' ] ; then
     #cd _site
     git init
         
-    git remote add deploy "deploy@139.59.36.122:/var/www/at.labs.appiume.com"
+    git remote add deploy "ssh://deploy@139.59.36.122:/var/www/at.labs.appiume.com/site.git"
     git config user.name "Linh Le"
     git config user.email "dev.linhlv@gmail.com"
     

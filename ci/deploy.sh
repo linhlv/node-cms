4a491ec809358cdcfb91c763981cfccba8bdfc94
Linh Le <dev.linhlv@gmail.com>
2016-12-08 11:48:42 +0700

    Test deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,17 +2,15 @@
 echo "Deploying"
 set -x
 if [ $TRAVIS_BRANCH == 'at' ] ; then
-    # Initialize a new git repo in _site, and push it to our server.
-    #cd _site
     git init
-        
-    git remote add deploy "ssh://deploy@139.59.36.122:/var/www/at.labs.appiume.com/site.git"
+
+    git remote add staging "ssh://deploy@139.59.36.122:/var/www/at.labs.appiume.com/site.git"
     git config user.name "Linh Le"
     git config user.email "dev.linhlv@gmail.com"
     
     git add .
-    git commit -m "Deploy a"    
-    git push deploy master
+    git commit -m "Deploy"    
+    git push staging master
 else
     echo "Not deploying, since this branch isn't master."
 fi

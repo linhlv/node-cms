47620cd59d9029084ed77aad182c549ed8a9eb23
Linh Le <dev.linhlv@gmail.com>
2016-12-08 14:03:02 +0700

    git init,add, commit before push

--- code/ci/deploy.sh
+++ code/ci/deploy.sh
@@ -2,6 +2,9 @@
 echo "Deploying"
 set -x
 if [ $TRAVIS_BRANCH == 'at' ] ; then    
+    git init 
+    git add .
+    git commit -m "Deploy"
     git remote add staging "ssh://deploy@139.59.36.122:/var/www/at.labs.appiume.com/site.git" 
     git push staging master
 else

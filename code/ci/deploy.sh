2aabd6b1c993be6286383549e0ec73e3542ba888
Linh Le <dev.linhlv@gmail.com>
2016-12-09 00:25:54 +0700

    Try git pull

--- code/ci/deploy.sh
+++ code/ci/deploy.sh
@@ -8,6 +8,7 @@ if [ $TRAVIS_BRANCH == 'at' ] ; then
     git add .
     git commit -m "Deploy"
     git remote add staging "ssh://deploy@139.59.36.122:/var/www/at.labs.appiume.com/site.git" 
+    git pull 
     git push staging master
 else
     echo "Not deploying, since this branch isn't master."

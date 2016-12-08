d5a35787166af9b64239f0505a3602b905f30621
Linh Le <dev.linhlv@gmail.com>
2016-12-09 00:28:27 +0700

    force push

--- code/ci/deploy.sh
+++ code/ci/deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 echo "Deploying"
 set -x
-if [ $TRAVIS_BRANCH == 'at' ] ; then    
+if [ $TRAVIS_BRANCH == 'at' ] ; then
+    mkdir logs
+    echo 'logs' > .gitignore    
     git config --global user.email "dev.linhlv@gmail.com"
     git config --global user.name "Linh Le"    
     git init 
     git add .
     git commit -m "Deploy"
-    git remote add staging "ssh://deploy@139.59.36.122:/var/www/at.labs.appiume.com/site.git" 
-    git pull 
-    git push staging master
+    git remote add staging "ssh://deploy@139.59.36.122:/var/www/at.labs.appiume.com/site.git"
+    git push -f staging master
 else
     echo "Not deploying, since this branch isn't master."
 fi

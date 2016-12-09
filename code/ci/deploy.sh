659240d786830241123c04056083237ca31eab75
Linh Le <linh.le@smoovapp.com>
2016-12-09 14:55:11 +0700

    Build and deploy staging only

--- code/ci/deploy.sh
+++ code/ci/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Deploying"
 set -x
-if [ $TRAVIS_BRANCH == 'at' ] ; then
+if [ $TRAVIS_BRANCH == 'staging' ] ; then
     sudo mkdir logs    
     git config --global user.email "dev.linhlv@gmail.com"
     git config --global user.name "Linh Le"    
@@ -11,5 +11,5 @@ if [ $TRAVIS_BRANCH == 'at' ] ; then
     git remote add staging "ssh://deploy@139.59.36.122:/var/www/at.labs.appiume.com/site.git"
     git push -f staging master
 else
-    echo "Not deploying, since this branch isn't master."
+    echo "Not deploying, since this branch isn't staging."
 fi
--- code/ci/download_config.sh
+++ code/ci/download_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Downloading config"
 set -x
-if [ $TRAVIS_BRANCH == 'at' ] ; then    
+if [ $TRAVIS_BRANCH == 'staging' ] ; then    
     scp deploy@139.59.36.122:/var/configs/at.labs.appiume.com/config.staging.js config.js
 else
-    echo "Not deploying, since this branch isn't master."
+    echo "Not deploying, since this branch isn't staging."
 fi

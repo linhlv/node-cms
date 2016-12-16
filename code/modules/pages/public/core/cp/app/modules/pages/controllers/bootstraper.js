0936fb3373f9f13c9bfff79c1321b0f34d75c17b
Linh Le <dev.linhlv@gmail.com>
2016-12-16 09:57:14 +0700

    Step selected page type

--- code/modules/pages/public/core/cp/app/modules/pages/controllers/bootstraper.js
+++ code/modules/pages/public/core/cp/app/modules/pages/controllers/bootstraper.js
@@ -1,10 +1,12 @@
 define(['modules/pages/controllers/pages.ctrl',
 'modules/pages/controllers/pages.list.ctrl',
-'modules/pages/controllers/page.add.1.ctrl'
+'modules/pages/controllers/page.add.1.ctrl',
+'modules/pages/controllers/page.add.2.ctrl'
 ],
-    function (pages, pages_list_ctrl, page_add_1_ctrl) {
+    function (pages, pages_list_ctrl, page_add_1_ctrl, page_add_2_ctrl) {
         angular.module('pages.controllers', [])
             .controller('pages.ctrl', ['$scope', pages])
             .controller('pages.list.ctrl', ['$scope', pages_list_ctrl])
-            .controller('page.add.1.ctrl', ['$scope', '$state', '$sessionStorage', 'pages.svc', page_add_1_ctrl]);
+            .controller('page.add.1.ctrl', ['$scope', '$state', '$sessionStorage', 'pages.svc', page_add_1_ctrl])
+            .controller('page.add.2.ctrl', ['$scope', '$state', '$sessionStorage', 'pages.svc', page_add_2_ctrl]);
 });
--- code/modules/pages/public/core/cp/app/modules/pages/controllers/page.add.2.ctrl.js
+++ code/modules/pages/public/core/cp/app/modules/pages/controllers/page.add.2.ctrl.js
@@ -4,16 +4,14 @@
 define(function () {
     function c($scope, $state, $sessionStorage, pagesSvc){
         var vm = this;
-
+        
+        /*
         pagesSvc.getTypesAll().then(function(res){
             if(res && res.items){
                 vm.items = res.items;                    
             }            
         });
-
-        vm.next = function(item){            
-            $state.go('pages.add.2', {typeId : item._id});
-        };
+        */
 
     }
 

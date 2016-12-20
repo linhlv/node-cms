b088df3b127ca801b414a38b942e1f8a2cd290c7
Linh Le <linh.le@smoovapp.com>
2016-12-20 10:44:55 +0700

    reoganize

--- code/modules/templates/public/core/cp/app/modules/templates/controllers/templates-edit.ctrl.js
+++ code/modules/templates/public/core/cp/app/modules/templates/controllers/templates-edit.ctrl.js
@@ -6,12 +6,22 @@ define(function () {
         var vm = this, id = $stateParams.id;
         vm.includes = function(s){ return $state.includes(s); };
         vm.tabSelect = function(s){ $state.go('templates.edit.' + s, id); };
-        // https://ace.c9.io/#nav=about
-        templatesSvc.get(id).then(function(res){
-            if(res && res.data){
-                vm.data = res.data;
-            }            
-        });
+        vm.title = (id ? 'Editing' : 'New') + ' page template';
+        
+        if(id){
+            // https://ace.c9.io/#nav=about
+            templatesSvc.get(id).then(function(res){
+                if(res && res.data){
+                    vm.data = res.data;
+                }            
+            });
+        }       
+
+        /*
+        var editor = ace.edit("editor");
+        editor.setTheme("ace/theme/monokai");
+        editor.getSession().setMode("ace/mode/javascript"); 
+        */
     }
 
     return c;

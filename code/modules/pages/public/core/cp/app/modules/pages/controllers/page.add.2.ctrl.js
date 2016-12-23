4ae16394c9cf68cc1949fd140d40a326b5426e49
Linh Le <linh.le@smoovapp.com>
2016-12-23 16:34:59 +0700

    page add updating

--- code/modules/pages/public/core/cp/app/modules/pages/controllers/page.add.2.ctrl.js
+++ code/modules/pages/public/core/cp/app/modules/pages/controllers/page.add.2.ctrl.js
@@ -2,15 +2,27 @@
 // monitor common task change
 /// </summary>
 define(function () {
-    function c($scope, $state, $sessionStorage, $stateParams, templatesSvc, pagesSvc){
+    function c($scope, $state, $q, $sessionStorage, $stateParams, typesSvc, templatesSvc, pagesSvc){
         var vm = this;
 
         if($stateParams.typeId){
-            templatesSvc.getAll().then(function(res){
-                if(res && res.items){
-                    vm.items = res.items;                    
-                }            
-            });
+
+            var callback = function(value){
+                return value;
+            };
+
+            var promiseGetType = typesSvc.get($stateParams.typeId).then(callback);
+            var promiseGetTemplates = templatesSvc.getAll().then(callback);
+
+             $q.all([promiseGetType, promiseGetTemplates]).then(
+                function(values){
+                    if(values && values.length === 2){
+                        vm.data = {};
+                        vm.data.type = values[0].data;
+                        vm.items = values[1].items;     
+                    }
+                }
+            );
         }
         
         vm.next = function(item){            
